922e815353e0c3f8b3a4a7d59117e486f9f5e74d
Sam <samgrover221692@gmail.com>
2023-03-10 06:23:37 -0800

    Initial Commit

---
+++ Cat or Dog?/Cat_or_Dog_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_or_Dog_App.swift
+//  Cat or Dog?
+//
+//  Created by Sam Grover on 2023-03-10.
+//
+
+import SwiftUI
+
+@main
+struct Cat_or_Dog_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
